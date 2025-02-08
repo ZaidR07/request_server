4cc5b65fa896329a9a8eedcb7a131ec737832e2b
Zaid Saifur Rahman <zaidrahman342@gmail.com>
2025-02-08 13:40:08 +0530

    Port changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 const TARGET_URL = "https://billmaker-server.onrender.com/api/file"; 
 
 // Function to make requests every 2 minutes
